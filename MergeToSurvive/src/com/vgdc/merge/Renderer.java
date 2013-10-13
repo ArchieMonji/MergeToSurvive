2c32660cb6530ad6c44fc7898175e1ada93d5565
Archie Monji <amonji@uci.edu>
2013-10-12 21:42:14 -0700

    Fleshing out renderer

--- MergeToSurvive/src/com/vgdc/merge/Renderer.java
+++ MergeToSurvive/src/com/vgdc/merge/Renderer.java
@@ -5,7 +5,29 @@ import java.util.ArrayList;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public interface Renderer {
-	public void onRender(SpriteBatch batch, float delta);
-	public void setAnimations(ArrayList<Animation> animations);
+public class Renderer {
+	public ArrayList<Animation> animations = new ArrayList<Animation>();
+	
+	public float stateTime;
+	public Animation currentAnimation;
+	
+	public Entity entity;
+	private int state; 
+	
+	public void onRender(SpriteBatch batch, float delta){
+		batch.draw(currentAnimation.getKeyFrame(stateTime), entity.getPosition().x, entity.getPosition().y);
+		stateTime += delta;
+	}
+	public void setAnimations(ArrayList<Animation> animations){
+		this.animations = animations;
+	}
+	
+	public void setState(int state){
+		this.state = state;
+		this.stateTime = 0;
+	}
+	
+	public int getState(){
+		return state;
+	}
 }
