f8185055b30d2ac0d37496a7f6fb1c949a3e68a3
Archie Monji <amonji@uci.edu>
2013-10-15 02:38:37 -0700

    Fixed Issue with platform collisions with projectiles.
    See: MovingBody.checkAllPlatformCollisions()
    
    Issue was, when new entity was created, its collision was only being checked with entities created after itself. This means, when a new projectile is created, it will never check platform collision with platforms already present in the world -- Archie

--- MergeToSurvive/src/com/vgdc/merge/assets/ClassFinder.java
+++ MergeToSurvive/src/com/vgdc/merge/assets/ClassFinder.java
@@ -1,26 +1,34 @@
 package com.vgdc.merge.assets;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import javax.swing.JOptionPane;
-
 public class ClassFinder {
 	/**
-	 * Code adapted from:
-	 * http://dzone.com/snippets/get-all-classes-within-package
+	 * Code from: http://dzone.com/snippets/get-all-classes-within-package
+	 * 
+	 * Scans all classes accessible from the context class loader which belong
+	 * to the given package and subpackages.
+	 * 
+	 * Adapted from http://snippets.dzone.com/posts/show/4831 and extended to
+	 * support use of JAR files
+	 * 
+	 * @param packageName
+	 *            The base package
+	 * @param regexFilter
+	 *            an optional class name pattern.
+	 * @return The classes
 	 */
 	public static ArrayList<Class<?>> getClassesInPackage(String packageName) {
 		try {
 			ClassLoader classLoader = Thread.currentThread()
 					.getContextClassLoader();
+			assert classLoader != null;
 			String path = packageName.replace('.', '/');
 			Enumeration<URL> resources = classLoader.getResources(path);
 			List<String> dirs = new ArrayList<String>();
@@ -39,31 +47,41 @@ public class ClassFinder {
 		}
 	}
 
-	private static ArrayList<Class<?>> findClasses(String path,
-			String packageName) {
-		if (path.startsWith("file:") && path.contains("!")) {
-			try {
-				return findClassesJar(path, packageName);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			try {
-				return findClassesDesktop(path, packageName);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return null;
-	}
-
 	/**
-	 * Code adapted from:
-	 * http://dzone.com/snippets/get-all-classes-within-package
+	 * Code from: http://dzone.com/snippets/get-all-classes-within-package
+	 * 
+	 * Recursive method used to find all classes in a given path (directory or
+	 * zip file url). Directories are searched recursively. (zip files are
+	 * Adapted from http://snippets.dzone.com/posts/show/4831 and extended to
+	 * support use of JAR files
+	 * 
+	 * @param path
+	 *            The base directory or url from which to search.
+	 * @param packageName
+	 *            The package name for classes found inside the base directory
+	 * @param regex
+	 *            an optional class name pattern. e.g. .*Test
+	 * @return The classes
 	 */
-	private static ArrayList<Class<?>> findClassesDesktop(String path,
-			String packageName) throws Exception {
+	private static ArrayList<Class<?>> findClasses(String path, String packageName)
+			throws Exception {
 		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
+		if (path.startsWith("file:") && path.contains("!")) {
+			String[] split = path.split("!");
+			URL jar = new URL(split[0]);
+			ZipInputStream zip = new ZipInputStream(jar.openStream());
+			ZipEntry entry;
+			while ((entry = zip.getNextEntry()) != null) {
+				if (entry.getName().endsWith(".class")) {
+					String className = entry.getName().replaceAll("[$].*", "")
+							.replaceAll("[.]class", "").replace('/', '.');
+					if (className.startsWith(packageName)){
+						classes.add(Class.forName(className));
+					}
+				}
+			}
+			zip.close();
+		}
 		File dir = new File(path);
 		if (!dir.exists()) {
 			return classes;
@@ -74,44 +92,14 @@ public class ClassFinder {
 				// another package level
 				classes.addAll(findClasses(file.getAbsolutePath(), packageName
 						+ "." + file.getName()));
-			} else {
-				String fileName = file.getName();
-				System.out.println("F: " + fileName);
-				if (fileName.endsWith(".class")) {
-					String className = packageName + '.'
-							+ fileName.substring(0, fileName.length() - 6);
-					classes.add(Class.forName(className));
-				}
+			} else if (file.getName().endsWith(".class")) {
+				String className = packageName
+						+ '.'
+						+ file.getName().substring(0,
+								file.getName().length() - 6);
+				classes.add(Class.forName(className));
 			}
 		}
-		return classes;
-	}
-
-	/**
-	 * Code adapted from:
-	 * http://dzone.com/snippets/get-all-classes-within-package
-	 */
-	private static ArrayList<Class<?>> findClassesJar(String path,
-			String packageName) throws Exception {
-		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
-
-		if (path.startsWith("file:") && path.contains("!")) {
-			String[] split = path.split("!");
-			URL jar = new URL(split[0]);
-			ZipInputStream zip = new ZipInputStream(jar.openStream());
-			for (ZipEntry entry = null; entry != null; entry = zip
-					.getNextEntry()) {
-				if (entry.getName().endsWith(".class")) {
-					String className = entry.getName().replaceAll("[$].*", "")
-							.replaceAll("[.]class", "").replace('/', '.');
-					if (className.startsWith(packageName))
-						classes.add(Class.forName(className));
-				}
-			}
-
-			zip.close();
-		}
-
 		return classes;
 	}
 }
--- MergeToSurvive/src/com/vgdc/merge/screens/GameScreen.java
+++ MergeToSurvive/src/com/vgdc/merge/screens/GameScreen.java
@@ -14,7 +14,7 @@ import com.vgdc.merge.entities.rendering.PlatformRenderer;
 import com.vgdc.merge.world.World;
 
 public class GameScreen extends AbstractScreen {
-	public static final boolean SHOWHITBOXES = false;
+	public static final boolean SHOWHITBOXES = true;
 	
 	private SpriteBatch batch;
 	
